7c40dc2749c2b5c6e0e0b5013ced998fe8065635
majawal <majawal@kth.se>
2022-03-01 17:08:49 +0100

    Started with formatting function

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 """"Main kör hela skiten"""
+import re
 
 def format():
     """Opens and turns csv-file into usable format, returns two numpyarrays (just use lists instead, is already included)"""
-    pass
+    file=open("PVC-t0.CSV")
+    list=[]
+    list2=[]
+    for line in file:
+        line.strip()
+        x=re.findall("^\d,.*?,",line)
+        list.append(line)
+        list2.append(x)
+    print(list)
+    print(list2)
+
 
 def absorbance_converter(wavenr_list,transm_list):
     """If data not already shown as absorbance, converts from transmittance to absorbance. Returns two numpyarrays"""
@@ -16,3 +27,6 @@ def main():
     [wave,transm]=format()
     [wave,transm]=absorbance_converter(wave,transm)
     find_index(wave,transm,1)
+
+if  __name__ == "__main__":
+    main()
