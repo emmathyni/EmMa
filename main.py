355c0b2fa87e7dedd6b88a574228c7f6ef193f68
majawal <majawal@kth.se>
2022-03-01 15:47:51 +0100

    Added main skeleton and calculations file

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-#kex yo
-#kex yoyoyo
-print('Hello World')
+""""Main kör hela skiten"""
+
+def format():
+    """Opens and turns csv-file into usable format, returns two numpyarrays (just use lists instead, is already included)"""
+    pass
+
+def absorbance_converter(wavenr_list,transm_list):
+    """If data not already shown as absorbance, converts from transmittance to absorbance. Returns two numpyarrays"""
+    pass
+
+def find_index(wavenr_list,abso_list,mode):
+    """Start calculation of indexes. Mode indicates which index wanted(?). Prints wanted indices"""
+    pass
+
+def main():
+    [wave,transm]=format()
+    [wave,transm]=absorbance_converter(wave,transm)
+    find_index(wave,transm,1)
