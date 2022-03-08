7438a98bc146f19c6c6c242a1d27d28c410f8a37
majawal <majawal@kth.se>
2022-03-08 13:12:55 +0100

    Changes by emmat

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 """"Main kör hela skiten"""
 import re
+import math
 
-def format():
+def format(name):
     """Opens and turns csv-file into usable format, returns two numpyarrays (just use lists instead, is already included)"""
-    file=open("PVC-t0.CSV")
-    list=[]
-    list2=[]
-    list3=[]
+    file = open(name)
+    wave = []
+    values = []
     for line in file:
         line.strip()
-        x=re.findall("^\d,.*?,",line)
-        y=re.findall(",\d,.*?$",line)
-        x=x[0][:-1]
-        y=y[0][1:]
+        x = re.findall("^\d,.*?,", line)
+        y = re.findall(",\d,.*?$", line)
+        x = x[0][:-1]
+        y = y[0][1:]
         x = re.sub(",", ".", x)
         y = re.sub(",", ".", y)
         x_n=int(x[-1])
@@ -28,12 +28,38 @@ def format():
     return wave, values
 
 
-def absorbance_converter(wavenr_list,transm_list):
-    """If data not already shown as absorbance, converts from transmittance to absorbance. Returns two numpyarrays"""
-    pass
+
+
+def absorbance_converter(values, transmittance, percentage):
+    """Converts values to absorbance if needed. Takes three arguments, the list values to be converted,
+    a boolean value for transmittance and percentage. Returns a list with absorbance values"""
+    # relation between transmittance and absorbance is A = -log_10(T) when T is between 0 and 1
+    # when T is percentage relation is A = log_10(100) - log_10(T)
+    # taken from https://www.edinst.com/blog/the-beer-lambert-law/
+
+    # check to determine type of data in values
+
+    if transmittance and percentage:
+        for i in range(len(values)):
+            values[i] = 2 - math.log10(values[i])
+        return values
+
+    if transmittance and not percentage:
+        for i in range(len(values)):
+            values[i] = - math.log10(values[i])
+        return values
+
+    if not transmittance:
+        return values
+
 
 def find_index(wavenr_list,abso_list,mode):
     """Start calculation of indexes. Mode indicates which index wanted(?). Prints wanted indices"""
+    pass
+
+def user_interaction():
+    """Takes in the arguments wanted from the user and returns these"""
+    file = input("Please enter file name: ")
     pass
 
 def main():
